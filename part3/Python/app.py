b2b03ecfe28d081669e000ccc9038097bfdd7177
Unknown <cesaralbc@gmail.com>
2018-06-24 13:06:36 -0300

    Fix the menu width

--- part3/Python/app.py
+++ part3/Python/app.py
@@ -2,26 +2,27 @@ import tkinter as tk
 import menu
 import server
 
+# open the server connection
 server.openConnection()
 
 # create and configure the root
 root = tk.Tk()
 root.geometry("1000x600")
-tk.Grid.rowconfigure(root, 0, weight=1)
-tk.Grid.columnconfigure(root, 0, weight=1)
-tk.Grid.columnconfigure(root, 1, weight=1,)
 
 # create and configure the lateral menu contatiner
-menuContainer = tk.Frame(root)
-menuContainer.grid(row=0, column=0, sticky='nsew')
-tk.Grid.columnconfigure(menuContainer, 0, weight=1)
+menuContainer = tk.Frame(root, width=200)
+menuContainer.pack(side='left', fill=tk.Y)
+menuContainer.pack_propagate(0)
 
 # create and configure the content container
 content = tk.Frame(root)
-content.grid(row=0, column=1, sticky='nsew')
+content.pack(side='right', fill=tk.BOTH, expand=1)
 
 # fill the menu options
 menu.fillMenu(menuContainer, content)
 
+# start the tk loop to keep the interface open
 tk.mainloop()
+
+# close the connection after the interface is finished
 server.closeConnection()
